8733cdf5880c000baa5b99560779450044117173
yushulx <lingxiao1002@gmail.com>
2022-06-07 13:50:09 +0800

    Rename the project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'barcode-qrcode-app'`, () => {
+  it(`should have as title 'angular-barcode-qr-code-scanner'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('barcode-qrcode-app');
+    expect(app.title).toEqual('angular-barcode-qr-code-scanner');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('barcode-qrcode-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-barcode-qr-code-scanner app is running!');
   });
 });
